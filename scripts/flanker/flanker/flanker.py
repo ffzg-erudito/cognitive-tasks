1f817b44c21a741c830304d9188647d7a1008048
denis <denis.vlasicek@gmail.com>
2019-02-12 13:57:34 +0100

    restructured

--- scripts/flanker/flanker/flanker.py
+++ scripts/flanker/flanker/flanker.py
@@ -15,18 +15,63 @@ numTrials = 160
 fixCrossDuration = 500
 blankDuration = 1000
 
-factors = {'distractor': ['HHH', 'KKK', 'CCC', 'SSS', ''],
-           'target': ['H', 'K', 'C', 'S']}
+factors = {'congruency': ['congruent', 'incongruent', 'same', 'control']}
+
+factorLevels = {'congruent': ['HHHKHHH', 'KKKHKKK', 'CCCSCCC', 'SSSCSSS'],
+                'incongruent': ['CCCKCCC', 'CCCHCCC', 'HHHSHHH', 'HHHCHHH',
+                                'SSSKSSS', 'SSSHSSS', 'KKKSKKK', 'KKKCKKK'],
+                'same': ['HHHHHHH', 'KKKKKKK', 'SSSSSSS', 'CCCCCCC'],
+                'control': ['H', 'C', 'S', 'K']}
 
 # experiment setup
 experiment = design.Experiment(name='Flanker', foreground_colour=(0, 0, 0),
                                background_colour=(255, 255, 255),
                                text_font='Courier', text_size=22)
 
+# initialize experiment
+control.initialize(experiment)
+
+# prepare practice block
+practiceBlock = design.Block()
+practiceBlock.add_trials_full_factorial(factors, copies=8)
+
+for trial in practiceBlock.trials:
+    factorLevel = trial.get_factor('congruency')
+    stimulus = design.randomize.rand_element(factorLevels[factorLevel])
+    trial.add_stimulus(stimuli.TextLine(stimulus))
+    trial.preload_stimuli()
+
+# add practice block to experiment
+experiment.add_block(practiceBlock)
+
+# function used to check constratints on stimulus order
+def stimChecker(trials):
+    """Checks whether the distractor from trial N is the target in trial N+1.
+    Returns False if there is no such case, and True if there is."""
+    for i in range(0, len(trials) - 1):
+        stimCurrent = trials[i].stimuli[0]
+        stimNext = trials[i + 1].stimuli[0]
+
+        currentText = stimCurrent.text
+        nextText = stimNext.text
+
+        # if it's a control condition, skip, since there is no distractor
+        if stimCurrent in factorLevels.get('control'):
+            continue
+        elif currentText[0] == nextText[3]:
+            return True
+        else:
+            return False
 
 # initialize external block with trials
 trialGenerator = design.Block()
-trialGenerator.add_trials_full_factorial(factors, copies=8)
+trialGenerator.add_trials_full_factorial(factors, copies=40)
+
+for trial in trialGenerator.trials:
+    factorLevel = trial.get_factor('congruency')
+    stimulus = design.randomize.rand_element(factorLevels[factorLevel])
+    trial.add_stimulus(stimuli.TextLine(stimulus))
+    trial.preload_stimuli()
 
 # shuffle trials
 loopFlag = True
@@ -38,20 +83,8 @@ while loopFlag:
 trialsBlocked = [trialGenerator.trials[i:i + 40]
                  for i in range(0, len(trialGenerator.trials), 40)]
 
-
-# functions
-def stimChecker(trials):
-    """Checks whether the distractor from trial N is the target in trial N+1.
-    Returns False if there is no such case, and True if there is."""
-    for i in range(0, len(trials) - 1):
-        distractorCurrent = trials[i].factor_dict.get('distractor')
-        targetNext = trials[i + 1].factor_dict.get('target')
-
-        # if the current distractor is blank, skip, since blank can't be a
-        # target
-        if distractorCurrent == '':
-            continue
-        elif distractorCurrent[0] == targetNext:
-            return True
-        else:
-            return False
+for trialBlock in trialsBlocked:
+    block = design.Block()
+    for trial in trialBlock:
+        block.add_trial(trial)
+    experiment.add_block(block)
