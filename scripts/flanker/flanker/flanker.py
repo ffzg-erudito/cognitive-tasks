7b380ed1e8eca7891b1761a2668dd97104c7eb86
denis <denis.vlasicek@gmail.com>
2019-02-13 19:55:17 +0100

    started building main loop

--- scripts/flanker/flanker/flanker.py
+++ scripts/flanker/flanker/flanker.py
@@ -2,7 +2,7 @@
 The relations among inhibition and interference control functions: a
 latent-variable analysis. Journal of experimental psychology: General, 133(1),
 101."""
-from expyriment import control, design, io, stimuli
+from expyriment import control, design, stimuli, misc
 
 from flanker.helpers import stimChecker, conditionChecker
 
@@ -10,9 +10,9 @@ from flanker.helpers import stimChecker, conditionChecker
 control.set_develop_mode(True)
 
 # experiment variables
-trialsPerCondInTest = 10
-trialPerCondInPractice = 8
-numTestBlocks = 4
+trialsPerCondInTest = 4
+trialPerCondInPractice = 2
+numTestBlocks = 2
 numPracticeBlocks = 1
 
 fixCrossDuration = 500
@@ -26,6 +26,8 @@ factorLevels = {'congruent': ['HHHKHHH', 'KKKHKKK', 'CCCSCCC', 'SSSCSSS'],
                 'same': ['HHHHHHH', 'KKKKKKK', 'SSSSSSS', 'CCCCCCC'],
                 'control': ['H', 'C', 'S', 'K']}
 
+keyDict = {'121': 'K', '109': 'M'}
+
 # experiment setup
 experiment = design.Experiment(name='Flanker', foreground_colour=(0, 0, 0),
                                background_colour=(255, 255, 255),
@@ -36,16 +38,18 @@ control.initialize(experiment)
 
 # set variable names
 experiment.add_data_variable_names(['subject', 'condition', 'reactionTime',
-                                    'stimulus', 'response'])
+                                    'stimulus', 'response', 'blockName'])
 
 
 for i in range(0, numTestBlocks + numPracticeBlocks):
     if i < numPracticeBlocks:
         trialsToAdd = trialPerCondInPractice
+        blockName = 'practice'
     else:
         trialsToAdd = trialsPerCondInTest
+        blockName = 'test'
 
-    block = design.Block()
+    block = design.Block(blockName)
 
     stimCheckFlag = True
     condCheckFlag = False
@@ -70,20 +74,32 @@ for i in range(0, numTestBlocks + numPracticeBlocks):
 
     experiment.add_block(block)
 
+for block in experiment.blocks:
+    for trial in block.trials:
+        trial.preload_stimuli()
+
 # preparing global stimuli
 blank = stimuli.BlankScreen()
 blank.preload()
 fixCross = stimuli.FixCross()
 fixCross.preload()
-responseInput = io.TextInput("")
+
+# TODO: dodati uputu, složiti unos šifre
 
 # start experiment
 control.start()
-blank.present()
-experiment.clock.wait(blankDuration)
 # main loop
 for block in experiment.blocks:
     for trial in block.trials:
+        blank.present()
+        experiment.clock.wait(blankDuration)
+        fixCross.present()
+        experiment.clock.wait(fixCrossDuration)
         trial.stimuli[0].present()
-        experiment.clock.wait(500)
+        key, rt = experiment.keyboard.wait([misc.constants.K_y,
+                                            misc.constants.K_m])
+        experiment.data.add([experiment.subject,
+                             trial.get_factor('congruency'), rt,
+                             trial.stimuli[0].text, keyDict[str(key)],
+                             block.name])
 control.end()
