c2aa3f4f61e896ebe91de8d120c9bb8f15cb332e
denis <denis.vlasicek@gmail.com>
2019-02-19 21:19:40 +0100

    finish stim creation for task switching

--- scripts/task_switching/task_switching/trials/trial_generator_helpers.py
+++ scripts/task_switching/task_switching/trials/trial_generator_helpers.py
@@ -5,20 +5,29 @@ def checkStim(stimulus, task, rowIndex, factorStimuli, response,
               responseTracker, numTracker):
 
     if response not in responseTracker and len(responseTracker) == 0:
-        responseTracker[response] += 1
+        increase = True
+        recreate = False
     elif response not in responseTracker and len(responseTracker) != 0:
-        responseTracker = Counter()
-        responseTracker[response] += 1
+        increase = True
+        recreate = True
     elif response in responseTracker and responseTracker[response] <= 2:
-        responseTracker[response] += 1
+        increase = True
+        recreate = False
     elif response in responseTracker and responseTracker[response] > 2:
         return True, responseTracker
 
     if str(stimulus) not in numTracker:
         numTracker[str(stimulus)] = rowIndex
+    elif rowIndex < 3 and str(stimulus) in numTracker:
+        return True, responseTracker
     elif rowIndex >= 3 and (rowIndex - numTracker[str(stimulus)]) < 3:
         return True, responseTracker
     else:
         numTracker[str(stimulus)] = rowIndex
+
+    if recreate:
+        responseTracker = Counter()
+    if increase:
+        responseTracker[response] += 1
 
     return False, responseTracker
